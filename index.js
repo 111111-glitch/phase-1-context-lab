0464611d152401c6d113cb910652b70992c5e3ac
Melissa <melissa.wanjiku@student.moringaschool.com>
2024-01-14 16:25:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -21,3 +21,85 @@ const allWagesFor = function () {
     return payable
 }
 
+// helpers.js
+
+// Create employee record
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // Create employee records from array of arrays
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+  }
+  
+  // Create timeIn event
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  // Create timeOut event
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  // Calculate hours worked on a specific date
+  function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find((event) => event.date === date);
+    const timeOut = this.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  // Calculate wages earned on a specific date
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  // Calculate all wages for an employee
+  function allWagesFor() {
+    const dates = this.timeInEvents.map((event) => event.date);
+    return dates.reduce((total, date) => total + wagesEarnedOnDate.call(this, date), 0);
+  }
+  
+  // Calculate all wages for all employees
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+  }
+  
+  // Find employee by first name in a collection
+  function findEmployeeByFirstName(collection, firstName) {
+    return collection.find((employee) => employee.firstName === firstName);
+  }
+  
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll,
+    findEmployeeByFirstName,
+  };
+  
